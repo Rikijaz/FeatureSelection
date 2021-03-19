e6b8d2fb32c6406d7a38645074a26e5e411ecf78
rikijaz <paperhearts34@gmail.com>
2021-03-18 22:41:01 -0700

    Search implemented with stub cross validation accuracy

--- FeatureSelection/Scripts/Program.cs
+++ FeatureSelection/Scripts/Program.cs
@@ -15,23 +15,78 @@ namespace FeatureSelection.Scripts
 			(Dictionary<uint, List<Datum>> smallDataByFeature,
 				Dictionary<uint, List<Datum>> largeDataByFeature) = DataBuilder.BuildAllData();
 
-			foreach (KeyValuePair<uint, List<Datum>> pair in smallDataByFeature)
+			Search(smallDataByFeature);
+
+			// foreach (KeyValuePair<uint, List<Datum>> pair in smallDataByFeature)
+			// {
+			// 	List<Datum> data = pair.Value;
+			//
+			// 	Console.WriteLine($"Feature '{pair.Key}'");
+			//
+			// 	if (pair.Key >= 10)
+			// 	{
+			// 		return;
+			// 	}
+			//
+			// 	for (int i = 0; i < data.Count; i++)
+			// 	{
+			// 		Console.WriteLine(
+			// 			$"Class: '{data[i].ClassValue}' Feature: '{data[i].Feature}' FeatureValue: '{data[i].FeatureValue}'");
+			// 	}
+			// }
+		}
+
+		private static void Search(Dictionary<uint, List<Datum>> dataByFeature)
+		{
+			HashSet<uint> unselectedFeatures = new HashSet<uint>(dataByFeature.Keys);
+
+			HashSet<uint> selectedFeatures = new HashSet<uint>();
+
+			uint level = 1;
+
+			while (unselectedFeatures.Count > 0)
 			{
-				List<Datum> data = pair.Value;
+				Console.WriteLine($"Traversing search tree level '{level}' ...");
+				uint selectedFeature = 0;
+				float selectedFeatureAccuracy = 0;
 
-				Console.WriteLine($"Feature '{pair.Key}'");
-
-				if (pair.Key >= 10)
+				foreach (uint unselectedFeature in unselectedFeatures)
 				{
-					return;
+					Console.WriteLine($"Considering adding feature '{unselectedFeature}' ...");
+
+					float accuracy =
+						CalculateCrossValidationAccuracy(dataByFeature[unselectedFeature]);
+
+					if (accuracy >= selectedFeatureAccuracy)
+					{
+						selectedFeatureAccuracy = accuracy;
+						selectedFeature = unselectedFeature;
+					}
 				}
 
-				for (int i = 0; i < data.Count; i++)
-				{
-					Console.WriteLine(
-						$"Class: '{data[i].ClassValue}' Feature: '{data[i].Feature}' FeatureValue: '{data[i].FeatureValue}'");
-				}
+				Console.WriteLine(
+					$"At search tree level '{level}', added feature '{selectedFeature}'.");
+
+				unselectedFeatures.Remove(selectedFeature);
+				selectedFeatures.Add(selectedFeature);
+				level++;
 			}
+
+			uint index = 1;
+
+			foreach (uint selectedFeature in selectedFeatures)
+			{
+				Console.WriteLine($"{index}. {selectedFeature}.");
+				index++;
+			}
+		}
+
+		private static float CalculateCrossValidationAccuracy(List<Datum> data)
+		{
+			const double range = float.MaxValue - (double) float.MinValue;
+			double value = new Random().NextDouble();
+
+			return (float) ((value * range) + float.MinValue);
 		}
 	}
 }
